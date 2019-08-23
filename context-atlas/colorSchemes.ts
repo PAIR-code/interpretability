a0b0e53a5e4b93467af61913e144fb47882ed16a
Emily Reif <ereif@google.com>
2019-08-23 13:24:18 -0400

    updating colors to match the PAIR guidlines from mahimap@

---
+++ context-atlas/colorSchemes.ts
@@ -0,0 +1,38 @@
+
+/**
+ * @license
+ * Copyright 2018 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+
+/**
+ * PAIR color schemes, in format comparable to d3.
+ */
+
+
+export const ColorSchemePastel = [
+  '#FFD8C3', '#87CDF9', '#FAF49F', '#CDC5FF', '#F6AEA9', '#BDF4E7', '#EDC4E6'
+];
+
+export const ColorSchemeBright = [
+  '#F0BD80', '#61AFF7', '#FFE839', '#9B86EF', '#FF777B', '#7DDAD3', '#EF96CD'
+];
+
+export const ColorSchemeDeep = [
+  '#FF9230', '#3C7DBF', '#D6A700', '#7647EA', '#FC4F61', '#1F978A', '#B22A72'
+];
+
+export const ColorSchemeDark = [
+  '#BA4A0D', '#184889', '#B48014', '#270086', '#B12D33', '#006067', '#632440'
+];
