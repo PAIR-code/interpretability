5c227b3ec489bb8a114ff153c80dcdbae9a76283
Alex Bäuerle <abauerle@google.com>
2020-07-14 12:27:25 +0200

    Build Version
    Uploaded a build version of the explainable.

---
+++ text-dream/interactive/precache-manifest.171a1c37fff51277cb9c01a8d8709865.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b88ae9b97d89127f465aedbe634f8126",
+    "url": "//interpretability/text-dream/interactive/index.html"
+  },
+  {
+    "revision": "4ee511ade5f93d8cc7c5",
+    "url": "//interpretability/text-dream/interactive/static/css/main.3100fe04.chunk.css"
+  },
+  {
+    "revision": "880ecccfb3330abcc013",
+    "url": "//interpretability/text-dream/interactive/static/js/2.a3a15d5d.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "//interpretability/text-dream/interactive/static/js/2.a3a15d5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ee511ade5f93d8cc7c5",
+    "url": "//interpretability/text-dream/interactive/static/js/main.ae1c4065.chunk.js"
+  },
+  {
+    "revision": "26d371624dbf8e815638fc351a19ea5e",
+    "url": "//interpretability/text-dream/interactive/static/js/main.ae1c4065.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ae7ffd2b7801967aef4",
+    "url": "//interpretability/text-dream/interactive/static/js/runtime-main.ab0d792c.js"
+  }
+]);
